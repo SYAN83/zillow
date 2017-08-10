cd2672a8991847aaa19bded60a58ba4c4f66974b
Shu Yan <yanshu.usc@gmail.com>
2017-08-10 09:03:33 +0000

    add ml train

--- eda.R
+++ eda.R
@@ -11,10 +11,12 @@ library(lubridate)
 library(VIM)
 
 # Import data
-prop <- fread(input = "https://s3.us-east-2.amazonaws.com/aws-emr-dedicated/data/zillow/properties_2016.csv")
-train <- fread(input = "https://s3.us-east-2.amazonaws.com/aws-emr-dedicated/data/zillow/train_2016_v2.csv")
+properties <- fread(input = "https://s3.us-east-2.amazonaws.com/aws-emr-dedicated/data/zillow/properties_2016.csv", 
+              na.strings = "")
+train <- fread(input = "https://s3.us-east-2.amazonaws.com/aws-emr-dedicated/data/zillow/train_2016_v2.csv",
+               na.strings = "")
 
-prop_train <- prop %>% filter(parcelid %in% train$parcelid)
+prop_train <- properties %>% filter(parcelid %in% train$parcelid)
 
 # EDA
 ## Transaction volumn by date
@@ -23,7 +25,7 @@ train %>%
                                 month=month(transactiondate))) %>% 
   group_by(year_month) %>% count() %>% 
   ggplot(aes(x=year_month, y=n)) +
-  geom_bar(stat="identity", fill="red") +
+  geom_bar(stat="identity", fill="blue", alpha=.5) +
   geom_vline(aes(xintercept=as.numeric(as.Date("2016-10-15"))), size=1)
 
 ## Distribution of logerror (99% percentile)
@@ -32,8 +34,8 @@ train %>%
                               quantile(train$logerror, .995))) %>%
   ggplot(aes(x=logerror)) +
   geom_histogram(aes(y=..density..), bins=50,
-                 color="black", fill="red", alpha=.5) + 
-  geom_density(alpha = .2, fill = "red")
+                 color="black", fill="blue", alpha=.5) + 
+  geom_density(alpha = .2, fill = "blue")
 
 ## Distribution of absolute logerror (99% percentile)
 train %>% 
@@ -42,20 +44,18 @@ train %>%
   mutate(abslogerr = abs(logerror)) %>%
   ggplot(aes(x=abslogerr)) +
   geom_histogram(aes(y=..density..), bins=50,
-                 color="black", fill="red", alpha=.5) + 
-  geom_density(alpha=.2, fill="red")
+                 color="black", fill="blue", alpha=.5) + 
+  geom_density(alpha=.2, fill="blue")
 
-## Mean of absolute logerror over time (99% percentile)
+## Mean of absolute logerror over time
 train %>% 
-  # filter(logerror %between% c(quantile(train$logerror, .005), 
-  #                             quantile(train$logerror, .995))) %>%
   mutate(year_month = make_date(year=year(transactiondate),
                                 month=month(transactiondate))) %>% 
   group_by(year_month) %>%
   summarise(meanabslogerr = mean(abs(logerror))) %>%
   ggplot(aes(x=year_month, y=meanabslogerr)) +
-  geom_line(color="red")+
-  geom_point(size=2, color="red")
+  geom_line(color="blue")+
+  geom_point(size=2, color="blue")
 ## Distribution of mean absolute logerror by month (99% percentile)
 train %>% 
   filter(logerror %between% c(quantile(train$logerror, .005),
@@ -64,13 +64,13 @@ train %>%
                                           month=month(transactiondate)))) %>% 
   mutate(abslogerr = abs(logerror)) %>%
   ggplot(aes(x=abslogerr)) +
-  geom_histogram(aes(y=..density..), alpha=.5, fill="red", bins=50) + 
+  geom_histogram(aes(y=..density..), alpha=.5, fill="blue", bins=50) + 
   facet_wrap(~ year_month)
 ## Missing percentage
 prop_train %>% 
   summarise_all(funs(sum(is.na(.))/n())) %>%
   gather(key="feature", value="missing_pct") %>%
   ggplot(aes(x=reorder(feature, missing_pct), y=missing_pct)) +
-  geom_bar(stat="identity",fill="red", alpha=.5)+
+  geom_bar(stat="identity",fill="blue", alpha=.5)+
   coord_flip()
 
--- ml.R
+++ ml.R
@@ -1,18 +1,60 @@
 library(caret)
+library(doMC)
+registerDoMC(cores = 4)
 
+## features with 75% or less missing values
 feature <- (prop_train %>% 
   summarise_all(funs(sum(is.na(.))/n())) %>%
   gather(key="feature", value="missing_pct") %>%
-  filter(missing_pct %between% c(0, .75)) %>%
+  filter(missing_pct == 0) %>%
+  # filter(missing_pct %between% c(0, .75)) %>%
   select(feature))$feature
-
+## train_data <- train inner_join properties 
 train_data <- train %>% 
   mutate(year=year(transactiondate),
          month=month(transactiondate)) %>%
   select(-transactiondate) %>%
-  inner_join(prop %>% select(feature), by="parcelid") 
+  inner_join(properties %>% select(feature), by="parcelid") 
+## factor columns
+factor_cols <- c("airconditioningtypeid", 
+                 "fips",
+                 "heatingorsystemtypeid", 
+                 "propertycountylandusecode",
+                 "propertylandusetypeid",
+                 "propertyzoningdesc", 
+                 "rawcensustractandblock",
+                 "regionidcity", 
+                 "regionidcounty", 
+                 "regionidneighborhood",
+                 "regionidzip",
+                 "censustractandblock")
+## convert columns to factors
+train_data <- train_data %>% mutate_at(.funs = as.factor, 
+                                       .vars = intersect(names(train_data), factor_cols))
+## near zero variance
+nzv <- nearZeroVar(train_data, saveMetrics= TRUE, freqCut = 99/1)
+train_data <- train_data[, !nzv$nzv]
+## factor columns
+factor_cols_rm <- (data.frame(lapply(train_data[, intersect(names(train_data), factor_cols)], 
+                  FUN = function(x) length(unique(x)))) %>%
+  gather(key="feature", value="count_unique") %>% 
+  filter(count_unique > 50))$feature
+## remove columns with more than 
+train_data <- train_data[, setdiff(names(train_data), factor_cols_rm)]
+## Data splitting based on the outcome
+set.seed(123)
+trainIndex <- createDataPartition(train_data$logerror, 
+                                  p = .75, 
+                                  list = FALSE, 
+                                  times = 1)
+subTrain <- train_data[ trainIndex,-1]
+subTest  <- train_data[-trainIndex,-1]
 
-str(train_data)
-
-nearZeroVar(prop_train[, cols$feature], saveMetrics= TRUE)
-
+fitCtrl <- trainControl(method = "cv",
+                        number = 3)
+gbmFit1 <- train(logerror ~ .,
+                 data = subTrain, 
+                 method = "gbm", 
+                 trControl = fitCtrl,
+                 verbose = FALSE)
+gbmFit1
